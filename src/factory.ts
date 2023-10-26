39f583202214bf9ff904c2df13bb408b4c790ad8
RainyLiao <liaowenrun@gmail.com>
2023-10-26 13:46:35 +0800

    :building_construction: 将cli拆分多个命令,以便未来支持更多的能力

--- src/factory.ts
+++ src/factory.ts
@@ -65,40 +65,50 @@ async function switchBranch(branch: string, direction: Direction, hash: string) 
 
 /**
  * Handles the arguments passed to the program and determines the target commit to view.
- * @param {string} dir - The directory to run the git commands in.
  * @param {object} opts - The options object containing the branch, direction, and hash properties.
  * @param {string} opts.branch - The name of the branch to view commits from.
  * @param {Direction} opts.direction - The direction to move through the commit history ('prev', 'next', or 'first').
  * @param {string} opts.hash - The hash of the commit to view.
  * @returns {Promise<void>} - A Promise that resolves when the target commit has been determined.
  */
-async function handleArguments(_: string, opts: RhOptions) {
+async function handleViewCommand(opts: RhOptions) {
   const { branch, direction, hash } = opts
   await checkBranch(branch)
   await switchBranch(branch, direction, hash)
 }
 
+
+async function handleBackCommand() {
+
+}
+
 export function main() {
   try {
-    sade('rh [dir]', true)
+    const prog = sade('rh');
+    prog
       .version('0.0.1')
-      .describe('帮助用户快速切换git的历史记录，查看源码')
-      .example('-b master -d prev')
-      .example('-b master -d next')
-      .example('-b master -d first')
+
+    prog
+      .command('view', '帮助用户快速切换git的历史记录，查看源码', { alias: 'v' })
+      .example('view -b master -d prev')
+      .example('view -b master -d next')
+      .example('view -b master -d first')
       .option('-b, --branch', '指定要查看源码的分支')
       .option('-h, --hash', '指定跳到某个具体的commit')
       .option('-d, --direction', '指定查看的方向，prev表示上一个，next表示下一个，first表示第一个')
-      .option('-v, --version', '查看版本号')
-      .option('-h, --help', '查看帮助')
-      .action(handleArguments)
-      .parse(process.argv, {
-        unknown: arg => chalk.red(`错误的参数或者参数值: ${arg}`),
-      })
-      // 增加一个rh back的命令
+    prog.action(handleViewCommand)
+
+    prog
+      .command('back', '回到被查看源码的分支', { alias: 'b' })
+    prog.action(handleBackCommand)
+
+    prog.parse(process.argv, {
+      unknown: arg => chalk.red(`错误的参数或者参数值: ${arg}`),
+    })
+    // 增加一个rh back的命令，用于回到被查看源码的分支
   }
   catch (p: any) {
     echo`${chalk.red('出错了')}`
-    echo`${chalk.red(`错误信息如下:  ${p.stderr}`)}`
+    echo`${chalk.red(`错误信息如下: \n${p}`)}`
   }
 }
